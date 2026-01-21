7dfdf68f34231ea686494dd0d73c97b401bd0188
unibox <unibox@duck.com>
2026-01-21 09:32:18 +0100

    add styles

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,6 @@ import './episodesData';
 // main.ts
 
 console.log('Bleach Marker');
+
+const episodeBtns = document.querySelectorAll('.episode-item');
+console.log('🚀 ~ :8 ~ episodeBtns:', episodeBtns);
